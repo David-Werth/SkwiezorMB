b9e9e3d29bbe4ed460ab629649d327236e7a2764
David <75678655+David-Werth@users.noreply.github.com>
2024-05-27 16:17:43 +0200

    chore: Refactor

---
+++ Source/GUI/PathProducer.h
@@ -0,0 +1,39 @@
+/*
+  ==============================================================================
+
+    PathProducer.h
+    Created: 27 May 2024 4:04:53pm
+    Author:  David Werth
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+#include "FFTDataGenerator.h"
+#include "AnalyzerPathGenerator.h"
+#include "../PluginProcessor.h"
+
+struct PathProducer
+{
+    PathProducer(SingleChannelSampleFifo<SkwiezorMBAudioProcessor::BlockType>& scsf) :
+    leftChannelFifo(&scsf)
+    {
+        leftChannelFFTDataGenerator.changeOrder(FFTOrder::order2048);
+        monoBuffer.setSize(1, leftChannelFFTDataGenerator.getFFTSize());
+    }
+    void process(juce::Rectangle<float> fftBounds, double sampleRate);
+    juce::Path getPath() { return leftChannelFFTPath; }
+private:
+    SingleChannelSampleFifo<SkwiezorMBAudioProcessor::BlockType>* leftChannelFifo;
+    
+    juce::AudioBuffer<float> monoBuffer;
+    
+    FFTDataGenerator<std::vector<float>> leftChannelFFTDataGenerator;
+    
+    AnalyzerPathGenerator<juce::Path> pathProducer;
+    
+    juce::Path leftChannelFFTPath;
+};
