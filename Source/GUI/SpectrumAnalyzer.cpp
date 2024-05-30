eeceded4576a872bf187e85ddcc8ea5e746c1aff
David <75678655+David-Werth@users.noreply.github.com>
2024-05-30 11:12:55 +0200

    bugfix: Fix analyzer text spacing padding

--- Source/GUI/SpectrumAnalyzer.cpp
+++ Source/GUI/SpectrumAnalyzer.cpp
@@ -74,21 +74,21 @@ void SpectrumAnalyzer::paint (juce::Graphics& g)
         drawFFTAnalysis(g, bounds);
     }
     
-    Path border;
-    
-    border.setUsingNonZeroWinding(false);
-    
-    border.addRoundedRectangle(getRenderArea(bounds), 4);
-    border.addRectangle(getLocalBounds());
-    
-    g.setColour(Colours::black);
+//    Path border;
+//    
+//    border.setUsingNonZeroWinding(false);
+//    
+//    border.addRoundedRectangle(getRenderArea(bounds), 4);
+//    border.addRectangle(getLocalBounds());
+//    
+//    g.setColour(Colours::black);
     
 //    g.fillPath(border);
     
     drawTextLabels(g, bounds);
     
-    g.setColour(Colours::orange);
-    g.drawRoundedRectangle(getRenderArea(bounds).toFloat(), 4.f, 1.f);
+//    g.setColour(Colours::orange);
+//    g.drawRoundedRectangle(getRenderArea(bounds).toFloat(), 4.f, 1.f);
 }
 
 std::vector<float> SpectrumAnalyzer::getFrequencies()
@@ -194,7 +194,7 @@ void SpectrumAnalyzer::drawTextLabels(juce::Graphics &g, juce::Rectangle<int> bo
 
         r.setSize(textWidth, fontHeight);
         r.setCentre(x, 0);
-        r.setY(1);
+        r.setY(bounds.getY() + 2);
         
         g.drawFittedText(str, r, juce::Justification::centred, 1);
     }
@@ -214,7 +214,7 @@ void SpectrumAnalyzer::drawTextLabels(juce::Graphics &g, juce::Rectangle<int> bo
         
         Rectangle<int> r;
         r.setSize(textWidth, fontHeight);
-        r.setX(getWidth() - textWidth);
+        r.setX(bounds.getRight() - textWidth - 2);
         r.setCentre(r.getCentreX(), y);
         
         g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::lightgrey );
@@ -224,7 +224,7 @@ void SpectrumAnalyzer::drawTextLabels(juce::Graphics &g, juce::Rectangle<int> bo
         str.clear();
         str << (gDb - 24.f);
 
-        r.setX(1);
+        r.setX(bounds.getX() + 2);
         textWidth = g.getCurrentFont().getStringWidth(str);
         r.setSize(textWidth, fontHeight);
         g.setColour(Colours::lightgrey);
